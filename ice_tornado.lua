b12271aa636aba296bd2d1f7c68774ac38825eb7
lexvue <alexandertvue@gmail.com>
2024-04-23 16:20:56 -0500

    Question 5 Commit: Added frigo

---
+++ ice_tornado.lua
@@ -0,0 +1,28 @@
+local delay = 100
+
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+combat:setParameter(COMBAT_PARAM_TICKS, 20000)
+combat:setArea(createCombatArea({
+{0, 0, 0, 1, 0, 0, 0},
+{0, 0, 1, 1, 1, 0, 0},
+{0, 1, 1, 1, 1, 1, 0},
+{1, 1, 1, 2, 1, 1, 1},
+{0, 1, 1, 1, 1, 1, 0},
+{0, 0, 1, 1, 1, 0, 0},
+{0, 0, 0, 1, 0, 0, 0}
+}))
+
+function onGetFormulaValues(player, level, maglevel)
+	local min = (level / 5) + (maglevel * 1.4) + 8
+	local max = (level / 5) + (maglevel * 2.2) + 14
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+
+	return combat:execute(creature, variant)
+end
