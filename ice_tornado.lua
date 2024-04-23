98b3ce541f9d6d4f50d1725c0f6f450fa329eeb5
Alexander Vue <111104139+lexvue@users.noreply.github.com>
2024-04-23 16:33:01 -0500

    cleaned up some unneeded code

--- ice_tornado.lua
+++ ice_tornado.lua
@@ -1,9 +1,6 @@
-local delay = 100
-
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
-combat:setParameter(COMBAT_PARAM_TICKS, 20000)
 combat:setArea(createCombatArea({
 {0, 0, 0, 1, 0, 0, 0},
 {0, 0, 1, 1, 1, 0, 0},
